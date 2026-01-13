85163d9f839a1098e9b03a4da9540dd8e5b0fd87
matthewsimpson <simpson.matthew.j97@gmail.com>
2026-01-13 15:12:33 +0000

    refactor(background.ts): added explict types

--- src/hooks/background.ts
+++ src/hooks/background.ts
@@ -1,20 +1,24 @@
 import {useEffect} from "react";
 import {useLocation} from "react-router";
 
-const backgroundClasses = {
+const backgroundClasses: {
+  destination: string;
+  crew: string;
+  technology: string;
+} = {
   destination: "destination",
   crew: "crew",
   technology: "technology",
 };
 
 function useBackgroundImage() {
-  const currentPath = useLocation().pathname;
+  const currentPath: string = useLocation().pathname;
   let backgroundClassname: string = "home";
 
   if (currentPath === "/") {
     backgroundClassname = "home";
   } else {
-    const currentPathParent = currentPath.split("/")[1];
+    const currentPathParent: string = currentPath.split("/")[1];
     console.log(currentPathParent);
     for (const [path, classname] of Object.entries(backgroundClasses)) {
       if (path === currentPathParent) {
@@ -22,7 +26,6 @@ function useBackgroundImage() {
       }
     }
   }
-  console.log(backgroundClassname);
 
   useEffect(() => {
     document.body.classList.add(backgroundClassname);
