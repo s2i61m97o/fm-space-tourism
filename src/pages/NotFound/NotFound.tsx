41bd235e8847d2745b4b83e52b80d8a03e2816d2
matthewsimpson <simpson.matthew.j97@gmail.com>
2026-01-14 09:02:45 +0000

    design(404): implement styling to 404 page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,6 +1,17 @@
+import clsx from "clsx";
 import {useBackgroundImage} from "../../hooks/useBackgroundImage";
+import styles from "./NotFound.module.scss";
 
 export default function NotFound() {
   useBackgroundImage();
-  return <h1>Page Not Found</h1>;
+  return (
+    <main className={clsx("container", styles.container)}>
+      <section className={styles.content}>
+        <h1 className={styles.title}>404: page not found</h1>
+        <p className={styles.body}>
+          the page you are looking for does not exist
+        </p>
+      </section>
+    </main>
+  );
 }
