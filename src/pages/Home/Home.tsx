8cd99ad93b9fd379dbb01a2e482e915a69f0b7f4
matthewsimpson <simpson.matthew.j97@gmail.com>
2026-01-18 15:25:28 +0000

    refactor: homepage to use BEM methodology

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <main className={styles.container}>
       <section className={styles.hero}>
-        <h2 className={styles.subtitle}>so, you want to travel to</h2>
-        <h1 className={styles.title}>space</h1>
+        <h2 className={styles.hero__subtitle}>so, you want to travel to</h2>
+        <h1 className={styles.hero__title}>space</h1>
         <p>
           Let's face it; if you want to go to space, you might as well genuinely
           got to outer space and not hover kind of on the edge of it. Well sit
